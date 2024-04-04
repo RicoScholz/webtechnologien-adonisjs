c857c12561c8e91d628b1dbb5fcfff2dc2f1b6da
Rico Scholz <rico.scholz@arcor.de>
2024-04-04 16:47:25 +0200

    style: add form validation and dark navigation

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,14 @@ function scrollToBottom(el) {
 const messages = document.getElementById('chat-messages');
 
 if (messages) scrollToBottom(messages);
+
+document.querySelectorAll('.needs-validation').forEach(form => {
+    form.addEventListener('submit', e => {
+        if (!form.checkValidity()) {
+            e.preventDefault()
+            e.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+    }, false)
+})
